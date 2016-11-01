1130d2e590c78e3cd9121a3469b9b807658bc82d
houshuai <120237979@qq.com>
2016-11-01 10:17:00 +0800

    添加第三方库文件

---
+++ haochang/haochang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  haochang
+//
+//  Created by 壹号商圈 on 16/11/1.
+//  Copyright © 2016年 com.houshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ haochang/haochang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  haochang
+//
+//  Created by 壹号商圈 on 16/11/1.
+//  Copyright © 2016年 com.houshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
